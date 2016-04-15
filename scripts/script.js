df84084491ea5e6d57e2a87217a926dff94a66f3
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-04-15 15:01:20 -0400

    adding prompt for user input, along with if / else conditions

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	
-	})
+	prompt('Please input your code');
+})
+
+
+var checkCoupon = function (prompt){
+  if(prompt === couponcode) {
+  console.log('Thank you! Your coupon has been applied');
+} else {
+  console.log('Sorry, this coupon is not valid');
+};
+};
+
+
+
+
